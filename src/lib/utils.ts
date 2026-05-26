4098951c12dd3a9ed56948e0803d5320b7859831
Alithiel31 <harmo.man.01@gmail.com>
2026-05-26 14:15:46 -0400

    project init with dockerfiles & docker-compose

--- src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-// 📁 src/lib/utils.ts
-// Petite utilité pour fusionner les classes (équivalent du cn de shadcn)
-export function cn(...classes: Array<string | false | null | undefined>) {
-return classes.filter(Boolean).join(" ");
-}
